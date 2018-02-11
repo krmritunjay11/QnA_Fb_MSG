258541ae89a7d403bf87a9e1b78f81f2fe1407e3
Mritunjay Kumar <mritunjay.kumar@careers360.com>
2018-02-11 12:26:08 +0530

    testing done

--- fb_qnabot/views.py
+++ fb_qnabot/views.py
@@ -91,24 +91,3 @@ class QnABotView(generic.View):
 #     #         return HttpResponse(self.request.GET['hub.challenge'])
 #     #     else:
 #     #         return HttpResponse('Error, invalid token')
-
-# class QnABotView(generic.View):
-#     # The get method is the same as before.. omitted here for brevity
-#     @method_decorator(csrf_exempt)
-#     def dispatch(self, request, *args, **kwargs):
-#         return generic.View.dispatch(self, request, *args, **kwargs)
-#
-#     # Post function to handle Facebook messages
-#     def post(self, request, *args, **kwargs):
-#         # Converts the text payload into a python dictionary
-#         incoming_message = json.loads(self.request.body.decode('utf-8'))
-#         # Facebook recommends going through every entry since they might send
-#         # multiple messages in a single call during high load
-#         for entry in incoming_message['entry']:
-#             for message in entry['messaging']:
-#                 # Check to make sure the received call is a message call
-#                 # This might be delivery, optin, postback for other events
-#                 if 'message' in message:
-#                     # Print the message to the terminal
-#                     pprint(message)
-#         return HttpResponse()
